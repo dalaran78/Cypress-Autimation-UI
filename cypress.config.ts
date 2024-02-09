cbeece0347c6fa312ecb71f53df27605294f71b2
Sergei Swift <sergei.swift@icloud.com>
2024-02-09 12:49:14 -0800

    fixed environment and added lesson textbox

---
+++ cypress.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "cypress";
+require('dotenv').config()
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "https://uitestingplayground.com",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    env:{
+        ...process.env,
+        stage:'https://stage.pasv.us',
+        prod:'https://coding.pasv.us',
+        demoQA:'https://demoqa.com',
+        herokuapp:'https://the-internet.herokuapp.com',
+    }
+  },
+ // viewportWidth: 1200,
+ // viewportHeight: 1400,
+  defaultCommandTimeout:16000,
+});
