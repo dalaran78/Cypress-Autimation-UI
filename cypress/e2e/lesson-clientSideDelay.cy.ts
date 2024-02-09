32268dca48c0b0644faeac7de0096461f114a491
Sergei Swift <sergei.swift@icloud.com>
2024-02-09 11:51:12 -0800

    lesson clientSideDelay

---
+++ cypress/e2e/lesson-clientSideDelay.cy.ts
@@ -0,0 +1,20 @@
+describe('CLIENT SIDE DELAY', () => {
+
+    it('test', () => {
+        cy.visit('/clientdelay');
+        cy.get('#ajaxButton')
+        .should('be.visible')
+        .and('exist').click();
+        cy.get('#spinner').should('be.visible').should('exist');
+       // cy.get('.be-success').contains('Button Triggering Client Side Logic');
+        cy.contains('.bg-success', 'Data calculated on the client side.');
+    });
+});
+
+//visit url
+//find button
+//assert button
+//click button
+//find spinner element(selector)
+//verify spinner loho(assert)
+//assert spinner logo(assert)
