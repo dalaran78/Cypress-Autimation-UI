b8661a3a1359164635d56f693f4989094aab5c15
Sergei Swift <sergei.swift@icloud.com>
2024-02-12 12:38:31 -0800

    refactored iFrame lessons and added new env record to config

--- pages/lessons_pages/IframeApp.ts
+++ pages/lessons_pages/IframeApp.ts
@@ -1,11 +1,15 @@
 class IFrameApp {
-
     getIframe(){
-
+        cy.frameLoaded('#mce_0_ifr');
+        cy.iframe('#mce_0_ifr').then(IFrameApp=>{
+            cy.wrap(IFrameApp).type("{selectAll}{del}").type('Test message!');
+            cy.wrap(IFrameApp).clear().type('Second test message!');
+        });
     }
+
     visit(){
         cy.visit(`${Cypress.env("herokuapp")}/iframe`);
     }
 }
 
-export const IFrameAppPage = new IFrameApp()
+export const IFrameAppPage = new IFrameApp();
